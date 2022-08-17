ffa88ef4510d3e74265fb2a6518fe5913ab18a0d
maromo71 <professormoraes@gmail.com>
2022-08-16 22:11:39 -0300

    Up do dia 16 de agosto

---
+++ aula02_1608/prjApolice/src/Apolice.java
@@ -0,0 +1,11 @@
+public class Apolice {
+    String nomeSegurado;
+    int idade;
+    float valorPremio;
+
+    void imprimir(){
+        System.out.println("Nome: " + nomeSegurado);
+        System.out.println("Idade: " + idade);
+        System.out.println("Valor: " + valorPremio);
+    }
+}
---
+++ aula02_1608/prjApolice/src/ApoliceTeste.java
@@ -0,0 +1,10 @@
+public class ApoliceTeste {
+    public static void main(String[] args) {
+        Apolice apolice = new Apolice();
+        apolice.nomeSegurado = "Ricardo da Silva";
+        apolice.idade = 15;
+        apolice.valorPremio = 1589.89f;
+        apolice.imprimir();
+
+    }
+}
