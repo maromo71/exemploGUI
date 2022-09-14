9bfe96ed3a9a543de2aa6d1472938ed7eb75ee60
maromo71 <professormoraes@gmail.com>
2022-09-13 22:29:43 -0300

    Aula do dia 13 de setembro

--- aula06_1309/prjFinanceiro/src/GerenciadorFinanceiro.java
+++ aula06_1309/prjFinanceiro/src/GerenciadorFinanceiro.java
@@ -1,5 +1,15 @@
+import com.xyz.financeiro.view.ContaView;
+
+import javax.swing.*;
+
 public class GerenciadorFinanceiro {
     public static void main(String[] args) {
-
+        JFrame tela = new JFrame();
+        tela.setContentPane(new ContaView().getPainelConta());
+        tela.setSize(500,500);
+        tela.setTitle("Controle de Contas");
+        tela.setLocationRelativeTo(null);
+        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        tela.setVisible(true);
     }
 }
--- aula06_1309/prjFinanceiro/src/com/xyz/financeiro/view/ContaView.java
+++ aula06_1309/prjFinanceiro/src/com/xyz/financeiro/view/ContaView.java
@@ -1,6 +1,11 @@
 package com.xyz.financeiro.view;
 
+import com.xyz.financeiro.business.ContaBus;
+import com.xyz.financeiro.model.Conta;
+
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ContaView {
     private JPanel painelConta;
@@ -14,4 +19,38 @@ public class ContaView {
     private JButton fimDePapoButton;
     private JLabel labelSaldo;
     private JLabel labelMensagem;
+
+    private Conta conta;
+    public ContaView() {
+
+        cadastrarContaButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int numConta = Integer.parseInt(textNumConta.getText());
+                String nome = textNomeCliente.getText();
+                conta = new Conta(numConta, nome, 0.0);
+                JOptionPane.showMessageDialog(null,
+                        "Conta cadastrada com sucesso",
+                        "Sucesso",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        depositarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                double valor = Double.parseDouble(textValorDeposito.getText());
+                ContaBus bus = new ContaBus(conta);
+                bus.depositar(valor);
+                JOptionPane.showMessageDialog(null,
+                        "Depósito efetuado com sucesso",
+                        "Sucesso",
+                        JOptionPane.INFORMATION_MESSAGE);
+                labelSaldo.setText(Double.toString(conta.getSaldo()));
+            }
+        });
+    }
+
+    public JPanel getPainelConta() {
+        return painelConta;
+    }
 }
